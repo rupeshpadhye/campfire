24774745a0d9513ae93411dfb52ad3e72b0ffb1b
Rupesh Padhye <rupeshpadhye@gmail.com>
2021-11-03 15:06:50 +0530

    intial example

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -8,7 +8,7 @@ import { useSession } from "next-auth/client";
 import prisma from "../../lib/prisma";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const post = await prisma.post.findOne({
+  const post = await prisma.post.findUnique({
     where: {
       id: Number(params?.id) || -1,
     },
